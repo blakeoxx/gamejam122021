ec0ca28db9d5e3529840f68ee62670852ab1526e
maxmoosa <maxmoosa@gmail.com>
2021-12-04 19:20:52 -0600

    Character rotation

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -10,8 +10,6 @@ public class playerController : MonoBehaviour
     [SerializeField] private float spinSpeed;
     private float hSpeed = 0;
     private float vSpeed = 0;
-
-    
     void Start()
     {
         
@@ -20,13 +18,29 @@ public class playerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        GameObject body = transform.Find("Body").gameObject;
         if (Input.GetKey(KeyCode.W) && vSpeed < walkSpeed)
         {
             vSpeed = vSpeed + frictionModifier;
-
+            
+            if (body.transform.rotation.eulerAngles.y > 0 && body.transform.rotation.eulerAngles.y < 180)
+            {
+                body.transform.Rotate(0,-spinSpeed,0);
+            } else if (body.transform.rotation.eulerAngles.y < 360)
+            {
+                body.transform.Rotate(0,spinSpeed,0);
+            }
+            
         } else if (Input.GetKey(KeyCode.S) && vSpeed > -walkSpeed)
         {
             vSpeed = vSpeed - frictionModifier;
+            if (body.transform.rotation.eulerAngles.y > 180)
+            {
+                body.transform.Rotate(0,-spinSpeed,0);
+            } else if (body.transform.rotation.eulerAngles.y < 180)
+            {
+                body.transform.Rotate(0,spinSpeed,0);
+            }
         }
         else if (vSpeed > 0)
         {
@@ -39,9 +53,26 @@ public class playerController : MonoBehaviour
         if (Input.GetKey(KeyCode.D) && hSpeed < walkSpeed)
         {
             hSpeed = hSpeed + frictionModifier;
+            
+            if (body.transform.rotation.eulerAngles.y > 90 && body.transform.rotation.eulerAngles.y < 270)
+            {
+                body.transform.Rotate(0,-spinSpeed,0);
+            } else if (body.transform.rotation.eulerAngles.y < 90 || body.transform.rotation.eulerAngles.y > 270)
+            {
+                body.transform.Rotate(0,spinSpeed,0);
+            }
+            
         } else if (Input.GetKey(KeyCode.A) && hSpeed > -walkSpeed)
         {
             hSpeed = hSpeed - frictionModifier;
+            
+            if (body.transform.rotation.eulerAngles.y > 270 || body.transform.rotation.eulerAngles.y < 90 )
+            {
+                body.transform.Rotate(0,-spinSpeed,0);
+            } else if (body.transform.rotation.eulerAngles.y < 270 && body.transform.rotation.eulerAngles.y > 90)
+            {
+                body.transform.Rotate(0,spinSpeed,0);
+            }
         }
         else if (hSpeed > 0)
         {
@@ -53,6 +84,7 @@ public class playerController : MonoBehaviour
         
         
         transform.Translate(hSpeed * Time.deltaTime,0,vSpeed * Time.deltaTime);
+        print("rotation = "+ body.transform.rotation.eulerAngles.y);
         
     }
 }
