f0ecdb07933dc248936457b42d87367f143f5243
blakeoxx <blakeoxx@netscape.net>
2022-10-02 21:35:20 -0500

    Updated billboard script to make enemies face camera

--- Assets/Scripts/Billboard.cs
+++ Assets/Scripts/Billboard.cs
@@ -15,6 +15,18 @@ public class Billboard : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(mainCamera.transform);
+        // Make the sprite face the camera. This doesn't account for slight differences in perspective views, but good enough for now
+        
+        float cameraFacing = mainCamera.transform.eulerAngles.y % 360;
+        float parentFacing = transform.parent.eulerAngles.y % 360;
+        Vector3 newRotation = transform.eulerAngles;
+        
+        // Clip the sprite rotation to either exactly left or right in relation to the camera
+        newRotation.y = (parentFacing > 180 ? 0 : 180) + cameraFacing;
+        
+        // Copy the camera rotation to appear to "stand up". Depending on the Y rotation, this might need to be inversed
+        newRotation.x = mainCamera.transform.eulerAngles.x * (parentFacing > 180 ? 1 : -1);
+
+        transform.eulerAngles = newRotation;
     }
 }
