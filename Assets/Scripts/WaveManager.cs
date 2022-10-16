0a231927b092c7832303f47e4c398caf70d69061
blakeoxx <blakeoxx@netscape.net>
2022-10-15 21:00:50 -0500

    Added wave overlay text

--- Assets/Scripts/WaveManager.cs
+++ Assets/Scripts/WaveManager.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WaveManager : MonoBehaviour
 {
+    [SerializeField] private Text waveTextOverlay;
     [SerializeField] private Wave[] waves;
     private int currentWave;
     private bool waveInProgress;
@@ -19,11 +21,43 @@ public class WaveManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.R) && !waveInProgress && currentWave < waves.Length)
+        // Update the overlay text
+        if (!waveInProgress)
         {
-            EnemySpawner.instance.SetWave(waves[currentWave]);
-            EnemySpawner.instance.SetCanSpawn(true);
-            waveInProgress = true;
+            if (currentWave < waves.Length)
+            {
+                waveTextOverlay.text = "Wave " + (currentWave + 1) + "/" + waves.Length + "\n" +
+                                       "Press <b>[R]</b> to start";
+            }
+            else
+            {
+                waveTextOverlay.text = "You beat the game!" + "\n" +
+                                       "Press <b>[R]</b> to restart from wave 1";
+            }
+        }
+        else
+        {
+            int enemiesLeft = waves[currentWave].waveSize - EnemySpawner.instance.GetWaveEnemyCount();
+            waveTextOverlay.text = "Wave " + (currentWave + 1) + "/" + waves.Length + "\n" +
+                                   enemiesLeft + " enemies left";
+        }
+        
+        // Start the wave if the start key is pressed
+        if (Input.GetKeyDown(KeyCode.R) && !waveInProgress)
+        {
+            if (currentWave < waves.Length)
+            {
+                EnemySpawner.instance.SetWave(waves[currentWave]);
+                EnemySpawner.instance.SetCanSpawn(true);
+                waveInProgress = true;
+            }
+            else
+            {
+                currentWave = 0;
+                EnemySpawner.instance.SetWave(waves[currentWave]);
+                EnemySpawner.instance.SetCanSpawn(true);
+                waveInProgress = true;
+            }
         }
     }
 
